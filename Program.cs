5e3bc1188c120301b6be6166a584b8a5a28dcbb2
Carlos Lozano <carlos@adaptive.me>
2019-05-03 15:02:20 +0200

    Work in progress.

---
+++ Program.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+
+
+namespace ThinkCode.Ocelot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
+
+
+            
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options =>
+                {
+                    options.Limits.MaxConcurrentConnections = 100;
+                    options.Limits.MaxConcurrentUpgradedConnections = 100;
+                    options.Limits.MaxRequestBodySize = 10 * 1024;
+                    options.Limits.MinRequestBodyDataRate = new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
+                    options.Limits.MinResponseDataRate =  new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
+                })
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config
+                        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("appsettings.json", true, true)
+                        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                        .AddJsonFile("ocelot.json", false, true)
+                        .AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                        .AddEnvironmentVariables();
+
+                })
+                .ConfigureServices(services =>
+                {
+                    services
+                        .AddOcelot()
+                        .AddConsul()
+                        .AddConfigStoredInConsul();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    //logging.ClearProviders();
+                    logging.AddSerilog();
+                    //Log.Logger = new LoggerConfiguration()
+                        //.MinimumLevel.Verbose()
+                        //.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                        //.MinimumLevel.Override("System", LogEventLevel.Warning)
+                        //.MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                        //.MinimumLevel.Override("Ocelot.Configuration.Repository.FileConfigurationPoller", LogEventLevel.Error)
+                        //.Enrich.FromLogContext()
+                        //.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}  {Message:lj}{NewLine}  {Exception}{NewLine}", theme: AnsiConsoleTheme.Literate)
+                     //   .CreateLogger();
+                })
+                .Configure(app =>
+                {
+                    app.UseOcelot().Wait();
+                });
+
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # Ocelot
 
 ![Ocelot](https://camo.githubusercontent.com/c2118a418c5805c899903bc34fcdf471c9edf0d5/687474703a2f2f74687265656d616d6d616c732e636f6d2f696d616765732f6f63656c6f745f6c6f676f2e706e67)
+
+dotnet dev-certs https --trust
+dotnet dev-certs https --help
+
+ASPNETCORE_ENVIRONMENT=Development
+ASPNETCORE_URLS=http://localhost:7000
+ASPNETCORE_URLS="http://localhost:6666;https://localhost:6667" dotnet run
+dotnet run --no-launch-profile
+
+{
+  "Logging": {
+    "LogLevel": {
+      "Default": "Information",
+      "Microsoft": "Warning",
+      "Microsoft.Hosting.Lifetime": "Information"
+    }
+  },
+  "AllowedHosts": "localhost",
+  "Kestrel": {
+    "EndPoints": {
+        "Http": {
+            "Url": "http://localhost:6000"
+        },
+        "Https": {
+            "Url": "https://localhost:6001"
+        }
+    }
+  }
+}
